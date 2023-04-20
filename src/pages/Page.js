1b7ed86dfd2bfb799e32fa3cc98ee9685c6366d5
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-04-20 10:27:50 +0000

    Add informations to details page

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { AiOutlineLeft } from 'react-icons/ai';
 import { getDetails } from '../redux/ItemSlice';
 import Details from '../components/Details';
+import Navbar from '../components/Navbar';
 
 const Page = () => {
   const { details } = useSelector((state) => state.items);
+  const { selected } = useSelector((state) => state.items);
 
   const navigate = useNavigate();
 
@@ -15,18 +18,37 @@ const Page = () => {
     dispatch(getDetails());
   }, [dispatch]);
 
-  // console.log(details);
-
   return (
-    <div>
-      <button type="button" onClick={() => navigate(-1)}>
-        Retour
-      </button>
-      <div>
+    <div className="informations">
+      <Navbar
+        action={(
+          <button className="back-btn" type="button" onClick={() => navigate(-1)}>
+            <AiOutlineLeft color="white" />
+          </button>
+)}
+        title="detail"
+      />
+      <div className="detailed">
+        <h1>{selected}</h1>
+      </div>
+      <div className="description">
+        <p>
+          Times serie of symbol
+          {' '}
+          {selected}
+          {' '}
+          again Euro from Monday 16th April 2023 to Thursday 20th April 2023
+        </p>
+      </div>
+      <table>
+        <tr>
+          <th>date</th>
+          <th>price</th>
+        </tr>
         {details.map((detail) => (
           <Details key={detail.key} date={detail.key} currency={detail} />
         ))}
-      </div>
+      </table>
     </div>
   );
 };
